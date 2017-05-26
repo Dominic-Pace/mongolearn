024e58ecfc41c32a8016d0f354096b938cfad2ac
Dominic-Pace <dpace@nhpace.net>
2017-05-25 22:47:04 -0400

    Continued structuring server. Added routes, middleware, and config paths

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import config from '../config';
+import middleware from '../middleware';
+import initializeDb from '../db';
+
+let router = express();
+
+//Connect to database
+initializeDb(db => {
+
+  //Internal middleware
+  router.use(middleware({ config, db }));
+
+  // API Routes v1
+
+});
+
+export default router;
